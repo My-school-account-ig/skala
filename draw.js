5c5258da96d60880cb72b841395c9fa5cd78f5c3
My-school-account-ig <27olrees@sjhs.newport.sch.uk>
2026-06-05 16:32:28 +0100

    Update draw.js

--- draw.js
+++ draw.js
@@ -2,4 +2,5 @@ const canvas = document.getElementById("canvas");
 
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "red";
+// google does not want to register this whyyy???
 ctx.fillRect(0, 0, 100, 100);
